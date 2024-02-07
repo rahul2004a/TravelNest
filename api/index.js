114c125ff44dcce08ee0710a48f6780667e0f4cc
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-02-07 17:10:59 +0530

    add schema for User, Hotel and Room

--- api/index.js
+++ api/index.js
@@ -1,6 +1,25 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!");
+});
+
 
 app.listen(8800, () => {
+    connect();
     console.log("Server is running on port 8800");
 })
