4d69c9e6dc4233960561a9b15c64c8e847d7e48d
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-02-08 16:40:28 +0530

    User Route and Hotel Route

--- api/index.js
+++ api/index.js
@@ -3,8 +3,8 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
-
-
+import hotelsRoute from "./routes/hotels.js";
+import authRoute from "./routes/auth.js";
 const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO);
@@ -18,6 +18,10 @@ mongoose.connection.on("disconnected", () => {
     console.log("mongoDB disconnected!");
 });
 
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/hotels", hotelsRoute);
 
 app.listen(8800, () => {
     connect();
---
+++ api/routes/hotels.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotels.js';
+
+const router = express.Router();
+
+router.post("/", createHotel);
+router.put("/:id", updateHotel);
+router.delete("/:id", deleteHotel);
+router.get("/find/:id", getHotel);
+router.get("/", getHotels);
+
+export default router;
