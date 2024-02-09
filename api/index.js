05c2d3b22ca67602adf05e67b57421bd3532cf9f
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-02-09 14:45:30 +0530

    add middleware

--- api/index.js
+++ api/index.js
@@ -5,6 +5,11 @@ dotenv.config();
 import mongoose from "mongoose";
 import hotelsRoute from "./routes/hotels.js";
 import authRoute from "./routes/auth.js";
+import cookieParser from 'cookie-parser';
+
+import usersRoute from "./routes/users.js";
+import cors from "cors";
+
 const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO);
@@ -18,10 +23,24 @@ mongoose.connection.on("disconnected", () => {
     console.log("mongoDB disconnected!");
 });
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/hotels", hotelsRoute);
+app.use("/api/users", usersRoute);
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+});
+
 
 app.listen(8800, () => {
     connect();
